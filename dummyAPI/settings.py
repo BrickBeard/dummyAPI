d31941684de379fbca73b1a67644fc413dd4eea4
BrickBeard <brickbeard.io@gmail.com>
2018-09-25 13:53:32 -0500

    Still trying to fix this static files issue with Heroku

--- dummyAPI/settings.py
+++ dummyAPI/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
