7377e51c2b4e3faf4c9889e3bfae82e508c77e12
brickbeard <brickbear.io@gmail.com>
2018-10-16 21:58:01 -0500

    Update production for db

--- dummyAPI/settings.py
+++ dummyAPI/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'dummyAPI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
